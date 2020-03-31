dcd6fc1e6c36abc65b8531b88437a7d6723e384e
Jay <jordanchu95@gmail.com>
2020-03-31 17:37:22 -0400

    fixing show page

---
+++ app/javascript/react/components/containers/CerealShow.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const CerealShow = ({ cerealInfo }) => {
+    let { id, name, description } = cerealInfo
+
+    return(
+        <p>
+            <span><strong>Name: </strong>{name}</span><br/>
+            <span><strong>Description: </strong>{description}</span>
+        </p>
+    )
+}
+
+export default CerealShow
--- app/javascript/react/components/containers/CerealsShowContainer.js
+++ app/javascript/react/components/containers/CerealsShowContainer.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import CerealShow from "./CerealShow"
+
 
 const CerealsShowContainer = (props) => {
+    const [ cerealInfo, setCerealInfo ] = useState({})
+    let cerealId = props.match.params.id
+
+    useEffect(() => {
+        fetch(`/api/cereals/${cerealId}`)
+            .then(response => {
+                if (response.ok) {
+                    return response
+                } else {
+                    throw new Error(`${response.status}: ${response.statusText}`)
+                }
+            })
+            .then(validatedResponse => validatedResponse.json())
+            .then(body => {
+                setCerealInfo(body.cereal)
+            })
+            .catch(error => {
+                console.log(`Error fetching cereal list: ${error.message}`)
+            })
+    }, [])
+
+
 
     return(
-        <div>show container!!</div>
+        <div>
+            <CerealShow
+                cerealInfo={cerealInfo}
+            />
+        </div>
     )
 }
 
